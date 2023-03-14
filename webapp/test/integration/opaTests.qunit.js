d81f7210ae8b6f1989243f3c0f801158e3e93339
Gabriela Benedetti <gabriela.benedetti@sap.com>
2023-03-14 14:22:39 +0000

    commit 1

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["dmsui/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"dmsui/test/unit/controller/main.controller"
+], function () {
+	"use strict";
+});
